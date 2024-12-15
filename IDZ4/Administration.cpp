7df75c5a132dca6dd72dfc55643ace612ea9f59a
Vova Babushkin <babushkinvova05@gmail.com>
2024-12-16 02:26:30 +0300

    buisness logic

--- IDZ4/Administration.cpp
+++ IDZ4/Administration.cpp
@@ -1,35 +1,46 @@
 #pragma once
-#include "Thread.cpp"
 #include "Client.cpp"
+#include "Thread.cpp"
+#include <iostream>
 #include <queue>
 #include <unistd.h> // dont work at windows
-#include <iostream>
 
-class Administration : public Thread{
-    public:
-        Administration(int _duration, int &rooms, std::queue<Client> &_waiters, pthread_mutex_t &mutex): work_duration{_duration}, rooms_number{rooms}, waiters{_waiters}, waiters_mutex(mutex) {}
-        virtual void run(){
-            pthread_mutex_t room_mutex;
-            std::cout<<"Modeling:: \n";
-            for(size_t cur_time = 0; cur_time < work_duration; ++cur_time){
-                std::cout<<"Day "<<cur_time<<":\n";
-                while(!waiters.empty() && rooms_number >0){
-                    pthread_mutex_lock(&waiters_mutex);
-                    if(waiters.empty())
-                        break;
-                    Client client= waiters.front();
-                    waiters.pop();
-                    pthread_mutex_unlock(&waiters_mutex);
-                    live_args args{rooms_number,room_mutex,cur_time, client.during, client.id};
-                    Client_live live{args};
-                    live.start();
-                }
-                sleep(1);
-            }
+class Administration : public Thread {
+public:
+  Administration(int _duration, int &rooms, std::queue<Client> &_waiters,
+                 pthread_mutex_t &mutex)
+      : work_duration{_duration}, rooms_number{rooms}, waiters{_waiters},
+        waiters_mutex(mutex) {}
+  virtual void run() {
+    pthread_mutex_t room_mutex;
+    pthread_mutex_t cout_mutex;
+    for (size_t cur_time = 0; cur_time < work_duration; ++cur_time) {
+
+      pthread_mutex_lock(&cout_mutex);
+      std::cout << "Day " << cur_time << ":\n";
+      pthread_mutex_unlock(&cout_mutex);
+
+      while (!waiters.empty() && rooms_number > 0) {
+        pthread_mutex_lock(&waiters_mutex);
+        if (waiters.empty()){
+            pthread_mutex_unlock(&waiters_mutex);
+            break;
         }
-    private:
-    int work_duration;
-    int &rooms_number;
-    std::queue<Client> &waiters;
-    pthread_mutex_t &waiters_mutex;
+        Client client = waiters.front();
+        waiters.pop();
+        pthread_mutex_unlock(&waiters_mutex);
+        live_args args{rooms_number,  room_mutex, cur_time,
+                       client.during, client.id,  cout_mutex};
+        Client_live live{args};
+        live.start();
+      }
+      sleep(1);
+    }
+  }
+
+private:
+  int work_duration;
+  int &rooms_number;
+  std::queue<Client> &waiters;
+  pthread_mutex_t &waiters_mutex;
 };
--- IDZ4/Client.cpp
+++ IDZ4/Client.cpp
@@ -1,49 +1,60 @@
 #pragma once
 #include "Thread.cpp"
+#include <iostream>
 #include <queue>
 #include <unistd.h> // dont work at windows
-#include <iostream>
 
-class Client: public Thread{
-    public:
-    int id;
-            int during;
-    Client(int _id,int _checkin, int _during, std::queue<Client> &_waiters, pthread_mutex_t &_mutex) : id{_id}, checkin_time{_checkin}, during{_during}, waiters{_waiters}, queue_mutex{_mutex}{}
-    virtual void run() {
-        sleep(checkin_time);
-        pthread_mutex_lock(&queue_mutex);
-        waiters.push(*this);
-        pthread_mutex_unlock(&queue_mutex);
-    }
+class Client : public Thread {
+public:
+  int id;
+  int during;
+  Client(int _id, int _checkin, int _during, std::queue<Client> &_waiters,
+         pthread_mutex_t &_mutex)
+      : id{_id}, checkin_time{_checkin}, during{_during}, waiters{_waiters},
+        queue_mutex{_mutex} {}
+  virtual void run() {
+    sleep(checkin_time);
+    pthread_mutex_lock(&queue_mutex);
+    waiters.push(*this);
+    pthread_mutex_unlock(&queue_mutex);
+  }
 
-    private:
-    int checkin_time;
-    std::queue<Client> &waiters;
-         pthread_mutex_t &queue_mutex;
+private:
+  int checkin_time;
+  std::queue<Client> &waiters;
+  pthread_mutex_t &queue_mutex;
 };
 
-struct live_args{
-        int &room_number;
-        pthread_mutex_t &mutex;
-        size_t cur_time;
-        int during;
-        int id;
-    };
+struct live_args {
+  int &room_number;
+  pthread_mutex_t &mutex;
+  size_t cur_time;
+  int during;
+  int id;
+  pthread_mutex_t &cout_mutex;
+};
 
-class Client_live: public Thread{
-    public:
-    Client_live(live_args _args) :args{_args} {}
-    virtual void run(){
-        std::cout<<"\tClient "<<args.id<<" check-in into his room for "<<args.during<<" days.\n";
-        pthread_mutex_lock(&args.mutex);
-        --args.room_number;
-        pthread_mutex_unlock(&args.mutex);
-        sleep(args.during);
-        pthread_mutex_lock(&args.mutex);
-        ++args.room_number;
-        pthread_mutex_unlock(&args.mutex);
-        std::cout<<"\tClient "<<args.id<<" leave his room.\n";
-    }
-    private:
-    live_args args;
+class Client_live : public Thread {
+public:
+  Client_live(live_args _args) : args{_args} {}
+  virtual void run() {
+    pthread_mutex_lock(&args.cout_mutex);
+    int id = args.id;
+    std::cout << "\tClient " << id << " check-in into his room for "
+              << args.during << " days.\n";
+    pthread_mutex_unlock(&args.cout_mutex);
+    pthread_mutex_lock(&args.mutex);
+    --args.room_number;
+    pthread_mutex_unlock(&args.mutex);
+    sleep(args.during);
+    pthread_mutex_lock(&args.mutex);
+    ++args.room_number;
+    pthread_mutex_unlock(&args.mutex);
+    pthread_mutex_lock(&args.cout_mutex);
+    std::cout << "\tClient " << id << " leave his room.\n";
+    pthread_mutex_unlock(&args.cout_mutex);
+  }
+
+private:
+  live_args args;
 };
