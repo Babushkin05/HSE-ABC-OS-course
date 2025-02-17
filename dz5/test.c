43449292dd007c7b9a6fd2df6f6c8aae6b83755c
Vova Babushkin <babushkinvova05@gmail.com>
2025-02-17 22:17:55 +0300

    akos

---
+++ dz5/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// cat /some/file | grep "needle"
+
+char* path = "./test.txt";
+
+int main() {
+    int fd[2];
+    pipe(fd);
+    pid_t son_id = fork();
+    if(son_id == 0){
+        int ffd = open(path, 0, 0666);
+        char buf[100];
+        int readed = read(ffd, buf, 100);
+        close(fd[0]);
+        write(fd[1], buf, strlen(buf) + 1);
+        close(fd[1]);
+    }
+    else{
+        wait(NULL);
+        char message[100];
+        read(fd[0], message, 100);
+        close(fd[0]);
+        char ans[100];
+        execl("grep", "grep", "needle", NULL);
+    }
+}
