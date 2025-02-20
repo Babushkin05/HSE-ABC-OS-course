1cf67380952086b3408de7bfc3b28649e0a2f986
Vova Babushkin <babushkinvova05@gmail.com>
2025-02-21 00:34:23 +0300

    dz 10

--- dz4/minicp.c
+++ dz4/minicp.c
@@ -2,12 +2,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int main(int argc, char* argv[]) {
     if(argc != 3){
         perror("program wait 2 filenames in flags: source and destination");
         return 1;
     }
+    struct stat file_stat;
+
     int fd1 = open(argv[1], O_RDONLY);
     
     if(fd1 < 0){
@@ -15,7 +20,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int fd2 = creat(argv[2], 0666);
+    if (fstat(fd1, &file_stat) == -1) {
+        perror("fstat");
+        close(fd1);
+        return 1;
+    }
+
+    mode_t old_umask = umask(0);
+    int fd2  = creat(argv[2], 0777 | (file_stat.st_mode & 0111));
+    umask(old_umask);
 
     char buffer[1] = {};
     int res;
