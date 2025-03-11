096c233e85eaf76093c5bb91e7dbb22ac4cfba07
Vova Babushkin <babushkinvova05@gmail.com>
2025-03-11 14:46:29 +0300

    9

---
+++ idz os 1/9/p1.c
@@ -0,0 +1,120 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <from> <to> <chan1> <chan2>\n", argv[0]);
+        exit(1);
+    }
+
+    char* from = argv[1];
+    char* to = argv[2];
+    char* readchan = argv[3];
+    char* writechan = argv[4];
+
+    if (mkfifo(readchan, 0666) == -1) {
+        perror("mkfifo");
+        exit(1);
+    }
+
+    if (mkfifo(writechan, 0666) == -1) {
+        perror("mkfifo");
+        exit(1);
+    }
+
+    // reading from file
+    int test_fd = open(from, O_RDONLY);
+    if (test_fd < 0) {
+        perror("open");
+        exit(1);
+    }
+    // writing to channel
+    int read_fd = open(readchan, O_WRONLY);
+    if (read_fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    char buffer[128];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(test_fd, buffer, sizeof(buffer))) > 0) {
+        if (bytes_read == -1) {
+            perror("read");
+            close(test_fd);
+            exit(1);
+        }
+
+        if (write(read_fd, buffer, bytes_read) == -1) {
+            perror("write");
+            close(read_fd);
+            exit(1);
+        }
+    }
+
+    if (close(test_fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    if (close(read_fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    int write_fd = open(writechan, O_RDONLY);
+    if (write < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    // write data to file
+    int fd = creat(to, 0666);
+    if (fd < 0) {
+        perror("create");
+        exit(1);
+    }
+
+    // read data from channel
+    char writebuf[128];
+
+    while ((bytes_read = read(write_fd, writebuf, sizeof(writebuf))) > 0) {
+        if (bytes_read == -1) {
+            perror("read");
+            close(write_fd);
+            exit(1);
+        }
+
+        if (write(fd, writebuf, bytes_read) == -1) {
+            perror("write");
+            close(fd);
+            exit(1);
+        }
+    }
+
+    if (close(write_fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+    if (close(fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    if (unlink(readchan) == -1) {
+        perror("unlink");
+        exit(1);
+    }
+
+    if (unlink(writechan) == -1) {
+        perror("unlink");
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ idz os 1/9/p2.c
@@ -0,0 +1,234 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int* find_all_numbers(char* string, ssize_t* ans_count) {
+    ssize_t cap = 10;
+    int* ans = (int*)malloc(cap * sizeof(int));
+    if (ans == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    ssize_t strcap = 10;
+    char* curstr =
+        (char*)malloc(strcap * sizeof(char));  // string of readed number
+    if (curstr == NULL) {
+        perror("malloc");
+        free(ans);
+        return NULL;
+    }
+
+    curstr[0] = '\0';
+    ssize_t curstr_len = 0;
+    *ans_count = 0;
+
+    for (ssize_t i = 0; string[i] != '\0'; ++i) {
+        if (string[i] >= '0' && string[i] <= '9') {
+            if (curstr_len + 1 >= strcap) {
+                strcap *= 2;
+                char* new_curstr =
+                    (char*)realloc(curstr, strcap * sizeof(char));
+                if (new_curstr == NULL) {
+                    perror("realloc");
+                    free(curstr);
+                    free(ans);
+                    return NULL;
+                }
+                curstr = new_curstr;
+            }
+            curstr[curstr_len] = string[i];
+            curstr[++curstr_len] = '\0';
+        } else {
+            if (curstr_len > 0) {
+                int a = atoi(curstr);
+                curstr_len = 0;
+                if (*ans_count >= cap) {
+                    cap *= 2;
+                    int* new_ans = (int*)realloc(ans, cap * sizeof(int));
+                    if (new_ans == NULL) {
+                        perror("realloc");
+                        free(curstr);
+                        free(ans);
+                        return NULL;
+                    }
+                    ans = new_ans;
+                }
+                ans[*ans_count] = a;
+                (*ans_count)++;
+            }
+        }
+    }
+
+    if (curstr_len > 0) {
+        int a = atoi(curstr);
+
+        if (*ans_count >= cap) {
+            cap *= 2;
+            int* new_ans = (int*)realloc(ans, cap * sizeof(int));
+            if (new_ans == NULL) {
+                perror("realloc");
+                free(curstr);
+                free(ans);
+                return NULL;
+            }
+            ans = new_ans;
+        }
+
+        ans[*ans_count] = a;
+        (*ans_count)++;
+    }
+
+    free(curstr);
+    return ans;
+}
+
+char* make_string_from_array(int* arr, ssize_t size) {
+    const int MAX_NUMBER_LENGTH = 12;  // 2^32 \sim 2 billoins
+
+    size_t buffer_size = 10 + (size * MAX_NUMBER_LENGTH) + (size * 2) + 1;
+    char* buffer = (char*)malloc(buffer_size);
+    if (buffer == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    sprintf(buffer, "there are %zu numbers: ", size);
+
+    for (size_t i = 0; i < size; i++) {
+        char number_str[MAX_NUMBER_LENGTH];
+        sprintf(number_str, "%d", arr[i]);
+        strcat(buffer, number_str);
+
+        if (i < size - 1) {
+            strcat(buffer, ", ");
+        }
+    }
+
+    return buffer;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <chan1> <chan2>\n", argv[0]);
+        exit(1);
+    }
+
+    char* readchan = argv[1];
+    char* writechan = argv[2];
+
+    // reading from channel
+    int read_fd = open(readchan, O_RDONLY);
+    if (read_fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+
+    const int CHUNC_SIZE = 128;
+    size_t buffer_size = CHUNC_SIZE;
+    size_t length = 0;
+    char* data = malloc(buffer_size * sizeof(char));
+
+    if (data == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    while (1)
+    {
+        ssize_t bytes_read = read(read_fd, data + length, CHUNC_SIZE);
+        
+        if (bytes_read == -1) {
+            perror("read");
+            free(data);
+            exit(1);
+        } else if (bytes_read == 0) {
+            if (length == 0) {
+                free(data);
+                exit(1);
+            } else {
+                break;
+            }
+        }
+
+        length += CHUNC_SIZE;
+
+        // there is '\0' in added string
+        char* endline_pos = memchr(data + (length - bytes_read), '\0', bytes_read);
+        if (endline_pos != NULL) {
+            length = endline_pos - data + 1;
+            break;
+        }
+
+        if (length >= buffer_size - 1) {
+            buffer_size *= 2;
+            data = realloc(data, buffer_size);
+            if (data == NULL) {
+                perror("realloc");
+                exit(1);
+            }
+        }
+    }
+    data[length] = '\0';
+    if (close(read_fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+    // process data
+    ssize_t cnt = 0;
+    int* arr = find_all_numbers(data, &cnt);
+    if (arr == NULL) {
+        perror("my func error");
+        exit(1);
+    }
+    char* ans = make_string_from_array(arr, cnt);
+    if (ans == NULL) {
+        perror("my func error");
+        free(arr);
+        exit(1);
+    }
+
+    // write data to second channel
+    int write_fd = open(writechan, O_WRONLY);
+    if (write_fd < 0) {
+        perror("open");
+        free(arr);
+        free(ans);
+        exit(1);
+    }
+
+    size_t offset = 0;
+    while (offset < length){
+        size_t bytes_to_write = length - offset;
+        if (bytes_to_write > CHUNC_SIZE) {
+            bytes_to_write = CHUNC_SIZE;
+        }
+
+        ssize_t bytes_written = write(write_fd, ans + offset, bytes_to_write);
+
+        if (bytes_written == -1) {
+            perror("write");
+            exit(1);
+        }
+
+        offset += bytes_written;
+    }
+
+    if (close(write_fd) < 0) {
+        perror("close");
+        free(arr);
+        free(ans);
+        exit(1);
+    }
+
+    free(arr);
+    free(ans);
+
+    return 0;
+}
