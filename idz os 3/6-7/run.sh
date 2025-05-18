5d887fc5ad7f324d6b78535b4957ecd7f7938a5c
Vova Babushkin <babushkinvova05@gmail.com>
2025-05-18 22:12:59 +0300

    6-7

---
+++ idz os 3/6-7/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER_IP="127.0.0.1"
+SERVER_PORT="8080"
+NUM_VISITORS="100"
+
+echo "Запуск $NUM_VISITORS посетителей..."
+
+for ((i=1; i<=$NUM_VISITORS; i++))
+do
+    ./client $SERVER_IP $SERVER_PORT $i &
+done
+
+echo "Все посетители запущены"
+wait
+echo "Все посетители завершили работу"
